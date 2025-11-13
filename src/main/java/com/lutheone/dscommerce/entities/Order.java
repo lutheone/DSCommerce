8f8fd0022657fe71ce95333161f5fff27e8bfc8c
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-11-13 16:06:53 -0300

    Payment

--- src/main/java/com/lutheone/dscommerce/entities/Order.java
+++ src/main/java/com/lutheone/dscommerce/entities/Order.java
@@ -25,6 +25,9 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
