bbc6222a11a509c063231ba76758b42ea9e43313
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-11-13 19:16:20 -0300

    OrderItem and OrderItemPK

--- src/main/java/com/lutheone/dscommerce/entities/Order.java
+++ src/main/java/com/lutheone/dscommerce/entities/Order.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -27,6 +30,9 @@ public class Order implements Serializable {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -70,6 +76,23 @@ public class Order implements Serializable {
     public void setStatus(OrderStatus status) {
         this.status = status;
     }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
+    }
+
 
     @Override
     public boolean equals(Object o) {
---
+++ src/main/java/com/lutheone/dscommerce/entities/OrderItem.java
@@ -0,0 +1,60 @@
+package com.lutheone.dscommerce.entities;
+
+import com.lutheone.dscommerce.entities.pk.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+}
