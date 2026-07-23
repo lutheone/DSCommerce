d5c9f514032432093946b07f1c50d1be8c51f9a7
Luiz Oliveira <contato3luizhenriqueoliveira@gmail.com>
2026-07-23 15:42:53 -0300

    add repositories

---
+++ src/main/java/com/lutheone/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.lutheone.dscommerce.repositories;
+
+import com.lutheone.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+}
