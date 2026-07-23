58628fee007f7eeecfe1d5349327c338bca0820e
Luiz Oliveira <contato3luizhenriqueoliveira@gmail.com>
2026-07-23 16:31:03 -0300

    implement product retrieval endpoint

--- src/main/java/com/lutheone/dscommerce/controllers/ProductController.java
+++ src/main/java/com/lutheone/dscommerce/controllers/ProductController.java
@@ -1,16 +1,27 @@
 package com.lutheone.dscommerce.controllers;
 
+import com.lutheone.dscommerce.entities.Product;
+import com.lutheone.dscommerce.repositories.ProductRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/products")
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductRepository repository;
+
+    public ProductController(ProductRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping
     public String test(){
-        return "ola, mundo!";
+        return repository.findById(1L)
+                .map(Product::getName)
+                .orElse("Produto não encontrado");
     }
 
 }
