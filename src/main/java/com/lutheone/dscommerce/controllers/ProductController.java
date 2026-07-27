1058167d2e27171270d46cfb0287944d002f6514
Luiz Oliveira <contato3luizhenriqueoliveira@gmail.com>
2026-07-27 17:42:20 -0300

    Implement product creation endpoint

--- src/main/java/com/lutheone/dscommerce/controllers/ProductController.java
+++ src/main/java/com/lutheone/dscommerce/controllers/ProductController.java
@@ -5,7 +5,11 @@ import com.lutheone.dscommerce.services.ProductService;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 @RestController
@@ -19,18 +23,25 @@ public class ProductController {
     }
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id) {
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> page = service.findAll(pageable);
+        return ResponseEntity.ok(page);
     }
 
     @PostMapping
-    public ProductDTO save(@RequestBody ProductDTO productDTO) {
-        return service.insert(productDTO);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO) {
+        ProductDTO result = service.insert(productDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(result.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(result);
     }
 
 }
